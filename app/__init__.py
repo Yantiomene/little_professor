9e77d9518a059abfd34f0d01443c7a7e3503f14b
Yantiomene <49885974+Yantiomene@users.noreply.github.com>
2024-08-10 17:01:21 +0100

    [Update] Add user_loader in init

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,6 @@ db = SQLAlchemy()
 migrate = Migrate()
 login_manager = LoginManager()
 
-
 def create_app(config_class='app.config.Config'):
     app = Flask(__name__)
     app.config.from_object(config_class)
@@ -17,7 +16,15 @@ def create_app(config_class='app.config.Config'):
     migrate.init_app(app, db)
     login_manager.init_app(app)
 
-    from app import routes, models
+    from app import routes
     app.register_blueprint(routes.bp)
 
     return app
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    from app.models import User
+    from app import db
+   
+    db.session.get(User, user_id)
