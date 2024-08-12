62e6083ea2907836f83ffca7ee71b1bed7bb469d
Yantiomene <49885974+Yantiomene@users.noreply.github.com>
2024-08-12 13:58:55 +0100

    [Feat] Adding axios to connect to the backend

---
+++ frontend/src/api/api.js
@@ -0,0 +1,51 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:5000/";
+
+export const registerUser = async (username) => {
+  try {
+    const response = await axios.post(`${API_URL}register`, {
+      username,
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Error registering user:", error);
+  }
+};
+
+export const loginUser = async (username) => {
+  try {
+    const response = await axios.post(`${API_URL}login`, {
+      username,
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Error logging in user:", error);
+  }
+};
+
+export const fetchQuestion = async (userId) => {
+  try {
+    const response = await axios.get(`${API_URL}question`, {
+      params: {
+        user_id: userId,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching question:", error);
+  }
+};
+
+export const submitAnswer = async (userId, problem, answer) => {
+  try {
+    const response = await axios.post(`${API_URL}answer`, {
+      user_id: userId,
+      problem,
+      answer,
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Error submitting answer:", error);
+  }
+};
