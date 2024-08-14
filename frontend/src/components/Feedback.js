e7b5f2c85349d029090787487edc1cbef7943a2b
Yantiomene <49885974+Yantiomene@users.noreply.github.com>
2024-08-14 15:00:44 +0100

    [Update] Adding a transition to the feedback component

--- frontend/src/components/Feedback.js
+++ frontend/src/components/Feedback.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Feedback = ({ feedback }) => {
+  const [showFeedback, setShowFeedback] = useState(false);
+
+  useEffect(() => {
+    if (feedback) {
+      setShowFeedback(true);
+      const timer = setTimeout(() => {
+        setShowFeedback(false);
+      }, 5000);
+
+      return () => clearTimeout(timer);
+    }
+  }, [feedback]);
+
   return (
     <div
       className={`feedback ${
         feedback.includes("Correct") ? "correct" : "incorrect"
-      }`}
+      } ${showFeedback ? "show" : ""}`}
     >
       {feedback}
     </div>
