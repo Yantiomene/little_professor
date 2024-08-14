79401e6833a455df4ffb091e13b5daed12cb47e4
Yantiomene <49885974+Yantiomene@users.noreply.github.com>
2024-08-14 14:28:54 +0100

    [Feat] Adding a footer component

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <p className="footer">© 2024 YanDev Team All rigths reserved</p>
+    </footer>
+  );
+};
+
+export default Footer;
